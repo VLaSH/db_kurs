a76c13ff0164e0d1d35b66e90bbc741573eef830
Vlad Shevtsov <vs@Vlads-MacBook-Pro.local>
2016-11-27 17:31:24 +0200

    Add controllers.

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,46 @@
+class DeliveriesController < ApplicationController
+  before_action :delivery, only: [:edit, :update, :destroy]
+
+  def new
+    @delivery = Delivery.new
+  end
+
+  def create
+    @delivery = Delivery.new(delivery_params)
+    if @delivery.valid? && @delivery.save
+      redirect_to deliveries_path
+    else
+      render :new, notice: 'Delivery created'
+    end
+  end
+
+  def update
+    if delivery.update(delivery_params)
+      redirect_to deliveries_path
+    else
+      render :edit, notice: 'Delivery Updated'
+    end
+  end
+
+  def destroy
+    delivery.destroy
+    redirect_to deliveries_path
+  end
+
+  private
+
+  def delivery_params
+    params.require(:delivery).permit(
+      :provider_id,
+      :product_id,
+      :price,
+      :amount,
+      :delivery_date,
+      :end_date
+    )
+  end
+
+  def delivery
+    @delivery ||= Delivery.find(params[:id])
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,47 @@ class Product < ApplicationRecord
   has_many :deliveries
   belongs_to :category
   has_one :availability
+
+  class << self
+    def connection
+      ActiveRecord::Base.connection
+    end
+
+    def _all
+      res = connection.execute("SELECT products.* FROM products")
+      make_hash(res.fields, res.values)
+    end
+
+    def _find(id)
+      res = connection.execute("SELECT products.* FROM products WHERE products.id = #{id} LIMIT 1")
+      make_hash(res.fields, res.values)
+    end
+
+    def _create(params)
+      res = connection.execute("INSERT INTO products (name, category_id) VALUES ('#{params[:name]}', '#{params[:category_id]}') RETURNING *")
+      make_hash(res.fields, res.values)
+    end
+
+    def _update(params, id)
+      str = ''
+      params.keys.each { |k| str << "#{k} = '#{params[k]}'," if params[k].present? }
+      str.chop!
+      connection.execute("UPDATE products SET #{str} WHERE products.id = #{id}")
+    end
+
+    def _destroy(id)
+      connection.execute("DELETE FROM products WHERE products.id = #{id}")
+    end
+
+    def make_hash(fields, values)
+      values.map do |v|
+        {
+          id: v[0],
+          name: v[1],
+          category_id: v[2]
+        }
+      end
+    end
+  end
+
 end
